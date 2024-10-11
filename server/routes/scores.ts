f1a0155f05eb9a08a9cbe8b973e097c9e3822bc7
Jared Pinfold <j.pinfold@gmail.com>
2024-10-12 11:33:36 +1300

    implement routes for mvp

---
+++ server/routes/scores.ts
@@ -0,0 +1,29 @@
+import { Router } from 'express'
+
+import * as db from '../db/scores.ts'
+
+const router = Router()
+
+router.get('/', async (req, res) => {
+  try {
+    const scores = await db.getAllScores()
+
+    res.json(scores)
+  } catch (error) {
+    console.log(error)
+    res.status(500).json({ message: 'Something went wrong' })
+  }
+})
+
+router.post('/', async (req, res) => {
+
+  try {
+    await db.addScore(req.body)
+    res.sendStatus(200)
+  } catch (error) {
+    console.log(error)
+    res.status(500).json({ message: 'Something went wrong' })
+  }
+})
+
+export default router
--- server/server.ts
+++ server/server.ts
@@ -1,13 +1,15 @@
 import express from 'express'
 import * as Path from 'node:path'
 
-import fruitRoutes from './routes/fruits.ts'
+import cryptids from './routes/cryptids.ts'
+import scores from './routes/scores.ts'
 
 const server = express()
 
 server.use(express.json())
 
-server.use('/api/v1/fruits', fruitRoutes)
+server.use('/api/v1/cryptids', cryptids)
+server.use('/api/v1/scores', scores)
 
 if (process.env.NODE_ENV === 'production') {
   server.use(express.static(Path.resolve('public')))
