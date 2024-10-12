1b609f5e24c6586ae634eaf2377f52d14906adb7
Jared Pinfold <j.pinfold@gmail.com>
2024-10-12 14:01:11 +1300

    created front end routes

--- server/db/seeds/cryptids.js
+++ server/db/seeds/cryptids.js
@@ -7,7 +7,7 @@ export async function seed(knex) {
     { id: 1, name: 'Lucas', size: 1, rage: 1, points: 10, image: '' },
     { id: 2, name: 'Kraken', size: 4, rage: 8, points: 80, image: '' },
     { id: 3, name: 'Cthulhu', size: 5, rage: 10, points: 100, image: '' },
-    { id: 4, name: 'Salad Fingers', size: 2, rage: 2, points: 20, image: '' },
+    { id: 4, name: 'Jason Statham', size: 2, rage: 3, points: 30, image: '' },
     { id: 5, name: 'Bunyip', size: 3, rage: 4, points: 40, image: '' }
   ])
 }
