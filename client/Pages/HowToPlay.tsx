93fa241ba1bedfaabeaaae93de68e2974f7795ca
Hannah Burgoyne <hannah.burgoyne@gmail.com>
2024-10-12 16:30:19 +1300

    style instructions page

--- client/Pages/HowToPlay.tsx
+++ client/Pages/HowToPlay.tsx
@@ -4,13 +4,18 @@ export default function HowToPlay() {
   const navigate = useNavigate()
 
   return (
-    <>
-      <h2>How to play:</h2>
-      <ul>
-        <li>Click on the things</li>
-        <li>{"Don't die"}</li>
+    <section className="m-10 flex h-fit flex-col items-center justify-center bg-black p-10 opacity-80">
+      <h2 className="font-heading text-xl text-red-600">How to play:</h2>
+      <ul className="p-8">
+        <li className="text-sm text-red-600">Click on the things</li>
+        <li className="text-sm text-red-600">{"Don't die"}</li>
       </ul>
-      <button onClick={() => navigate('/fishing')}>Play!</button>
-    </>
+      <button
+        className="rounded-md border-2 border-solid border-red-600 p-2 px-6 text-sm text-red-600"
+        onClick={() => navigate('/fishing')}
+      >
+        Play!
+      </button>
+    </section>
   )
 }
