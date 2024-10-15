7abde5f2ccee2785a14593828d9cfe0ef1af1be1
Daphne Simons <daphne.simons@devacademy.co.nz>
2024-10-15 16:32:19 +1300

    looks good - format donezo

--- server/db/seeds/scores.js
+++ server/db/seeds/scores.js
@@ -4,15 +4,15 @@ export async function seed(knex) {
 
   // Inserts seed entries
   await knex('scores').insert([
-    {id: 1, name: 'Hannah', score: 100},
-    {id: 2, name: 'Daph', score: 100},
-    {id: 3, name: 'Jared', score: 100},
-    {id: 4, name: 'Hooper', score: 10},
-    {id: 5, name: 'Brody', score: 10},
-    {id: 6, name: 'Quint', score: 10},
-    {id: 7, name: 'Joel', score: 5},
-    {id: 8, name: 'Mikey', score: 5},
-    {id: 9, name: 'Nadia', score: 5},
-    {id: 10, name: 'Luxo', score: -100}
+    { id: 1, name: 'Hannah', score: 100 },
+    { id: 2, name: 'Daph', score: 100 },
+    { id: 3, name: 'Jared', score: 100 },
+    { id: 4, name: 'Hooper', score: 10 },
+    { id: 5, name: 'Brody', score: 10 },
+    { id: 6, name: 'Quint', score: 10 },
+    { id: 7, name: 'Joel', score: 5 },
+    { id: 8, name: 'Mikey', score: 5 },
+    { id: 9, name: 'Nadia', score: 5 },
+    { id: 10, name: 'Luxo', score: -100 },
   ])
 }
